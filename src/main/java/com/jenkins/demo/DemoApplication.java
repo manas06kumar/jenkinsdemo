1106c3fc97f41ddc28cb3b2757510e62ba22908d
Manas Kumar <manas.06kumar@gmail.com>
2025-08-09 14:42:31 -0700

    Initial jenkinsdemo commit

---
+++ src/main/java/com/jenkins/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class DemoApplication {
+	
+	@GetMapping("jenkins")
+	public String getJenkins() {
+		return "Jenkins Pipeline Triggered";
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
